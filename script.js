9180c4914649ed435c0f71623ce6a412e6390764
sopectel <xsfd2jy2pj@privaterelay.appleid.com>
2025-12-26 22:00:56 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Small script to set the current year in the footer
-document.addEventListener('DOMContentLoaded', function () {
-  const y = document.getElementById('year');
-  if (y) y.textContent = new Date().getFullYear();
-});
